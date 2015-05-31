986b65029aef7f5cdd33cf62ab04ff4618044701
Denis-Carl Robidoux <deniscarl@gmail.com>
2015-05-31 01:14:35 -0400

    Very small buttons changes, Still thinking about architecture, but I got a feeling this will be a cool soft.

--- stabview.py
+++ stabview.py
@@ -1,57 +1,88 @@
 #!/usr/bin/python
-import pygame
-import Tkinter
-import tkFileDialog
-import os, sys
 
-from pygame.locals import *
+# import the pygame module, so you can use it
+import pygame, pygbutton
+import Tkinter, tkFileDialog, os, sys
 
-if not pygame.font: print 'Warning, fonts disabled'
-
-class PyManMain:
-    """The Main PyMan Class - This class handles the main 
-    initialization and creating of the Game."""
-    
-    def __init__(self, width=1280,height=720):
-        """Initialize"""
-        """Initialize PyGame"""
-        pygame.init()
-        """Set the window Size"""
-        self.width = width
-        self.height = height
-        """Create the Screen"""
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("La Gugusse 16mm - Picture stabilization")
-        self.font = pygame.font.Font(None, 36)
-        text=self.font.render("Hello There", 1, (80, 80, 80))
-        textpos=text.get_rect()
-	background = pygame.Surface(self.screen.get_size())
-	background = background.convert()
-	background.fill((250, 250, 250))
-	textpos.centerx = background.get_rect().centerx
-        textpos.centery = background.get_rect().centery
-        background.blit(text, textpos)
-        self.screen.blit(background, (0, 0))
-        pygame.display.flip()
-    def MainLoop(self):
-        """This is the Main Loop of the Game"""
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: 
-                    sys.exit()
- 
-
+screen=None
+buttons=[]
 tkroot=Tkinter.Tk()
 tkroot.withdraw()
-
 currdir=os.getcwd()
-#fromdir=tkFileDialog.askdirectory(parent=tkroot, initialdir=currdir, title='Please select the directory of the original pictures')
-#todir=tkFileDialog.askdirectory(parent=tkroot, initialdir=currdir, title='Now please select another directory for output')
+fromdir=None
+todir=None
 
-#if fromdir == todir:
-#    print "The input and output directories must not be the same (operation cancelled)"
-#    sys.exit(0)
+def addButton(caption, func):    
+    print "Adding button "+caption
+    newbutton=pygbutton.PygButton((10,30 + 40 * len(buttons), 200,30), caption)
+    buttons.append((newbutton, func))
+    
+def selInDir():
+    fromdir=tkFileDialog.askdirectory(parent=tkroot, initialdir=currdir, title='Please select the directory of the original pictures')
 
-MainWindow = PyManMain()
-MainWindow.MainLoop()
+def selOutDir():
+    todir=tkFileDialog.askdirectory(parent=tkroot, initialdir=currdir, title='Please select a directory for output')
 
+def selImage():
+    print "selImage"
+
+def setHoleCoord():
+    print "setHoleCoord"
+
+def setFrameCoord():
+    print "setFrameCoord"
+
+def setAudioCoord():
+    print "setAudioCoord"
+
+def changeAudioMode():
+    print "ChangeAudioMode"
+
+def render():
+    print "RENDER"
+
+
+# define a main function
+def main():
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("La Gugusse 16mm - Stabilization")
+    
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((1280,720))
+
+    addButton('Set In Directory', selInDir)
+    addButton('Set Out Directory', selOutDir)
+    addButton('Choose Ref. Pix', selImage)
+    addButton('Set Hole Coord.', setHoleCoord)
+    addButton('Set Frame Coord.', setFrameCoord)
+    addButton('Set Audio Coord.', setAudioCoord)
+    addButton('Audio Mode', changeAudioMode)
+    addButton('RENDER', render)
+    for but in buttons:
+        but[0].draw(screen)
+    pygame.display.flip()
+    
+    # define a variable to control the main loop
+    running = True
+
+    # main loop
+    while running:
+        # event handling, gets all event from the eventqueue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            for but in buttons:
+                if 'click' in but[0].handleEvent(event):
+                    but[1]()
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+    
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
